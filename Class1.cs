5692aee4ac9bcf24d023efcda7347746d488b61a
Alec Cowan <alec@roguesaloon.tech>
2017-06-22 14:55:25 +0100

    Initial Commit with current version of the project

---
+++ Class1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unbroken.LaunchBox.Plugins;
+using Unbroken.LaunchBox.Plugins.Data;
+
+namespace PCSX2_Configurator
+{
+    public class Class1 : IGameMenuItemPlugin
+    {
+        public string Caption
+        {
+            get
+            {
+                return "Configure PCSX2 Settings";
+            }
+        }
+
+        public System.Drawing.Image IconImage
+        {
+            get
+            {
+                return Properties.Resources.pcsx2.ToBitmap();
+            }
+        }
+
+        public bool ShowInBigBox
+        {
+            get
+            {
+                return false;
+            }
+        }
+
+        public bool ShowInLaunchBox
+        {
+            get
+            {
+                return true;
+            }
+        }
+
+        public bool SupportsMultipleGames
+        {
+            get
+            {
+                return false;
+            }
+        }
+
+        public bool GetIsValidForGame(IGame selectedGame)
+        {
+            if (PluginHelper.DataManager.GetEmulatorById(selectedGame.EmulatorId).Title == "PCSX2")
+                return true;
+
+            return false;
+        }
+
+        public bool GetIsValidForGames(IGame[] selectedGames)
+        {
+            return SupportsMultipleGames;
+        }
+
+        public void OnSelected(IGame[] selectedGames)
+        {
+           return;
+        }
+
+        public void OnSelected(IGame selectedGame)
+        {
+            var pcsx2 = PluginHelper.DataManager.GetEmulatorById(selectedGame.EmulatorId);
+            var safeTitle = selectedGame.Title;
+            foreach(var c in Path.GetInvalidFileNameChars())
+            {
+                safeTitle = safeTitle.Replace(c.ToString(),"");
+            }
+            var configPath = "inis\\" + safeTitle;
+            selectedGame.CommandLine = pcsx2.CommandLine + " --cfgpath \"" + configPath + "\"";
+            configPath = Path.GetDirectoryName(pcsx2.ApplicationPath) + "\\" + configPath;
+            if (!File.Exists(configPath + "\\PCSX2_ui.ini"))
+            {
+                Directory.CreateDirectory(configPath);
+                var uiConfigFile = File.CreateText(configPath + "\\PCSX2_ui.ini");
+                uiConfigFile.Write("[MemoryCards]\r\nSlot1_Enable=enabled\r\nSlot1_Filename=" + safeTitle.Replace(" ", "") + ".ps2");
+                uiConfigFile.Close();
+            }
+            Process.Start(pcsx2.ApplicationPath, "--cfgpath \"" +  configPath + "\"");
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,6 @@
+PCSX2 Configurator v0.9
+
+This is a configuration plugin for games being run in PCSX2, allowing the user to configure custom per-game configs from right within Launchbox.
+Click this option on a chosen game to create a custom game config (if one doesn't already exist), and configure pcsx2 settings for only that game.
+Each game also creates it's own custom memory card (slot 1), with a shared memory card being in memory card slot 2, all files and settings are stored
+in the PCSX2 emulator path.
